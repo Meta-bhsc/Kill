a1ceeb5cae3da2e42ab421b13db0705dbfaf60d0
KingKade1019 <152344982+KingKade1019@users.noreply.github.com>
2024-07-17 16:06:48 -0700

    Update KILL.py

--- KILL.py
+++ KILL.py
@@ -1,1 +1,106 @@
+import os
+import colorama
+from colorama import Fore
+import time
+#-------------------------DO-NOT-TOUCH-!!!-----------------------------------------------------------------------------------------
 
+os.system('clear')
+time.sleep(1)
+print(Fore.CYAN + "STARTING in 3")
+time.sleep(1)
+os.system('clear')
+time.sleep(1)
+print(Fore.CYAN + "STARTING in 2")
+time.sleep(1)
+os.system('clear')
+time.sleep(1)
+print(Fore.CYAN + "STARTING in 1")
+time.sleep(1)
+os.system('clear')
+#------------------------------------BOOT-UP---------------------------------------------------------------------------------
+print(Fore.BLACK + '!!!!!           !!!!!     '      + Fore.BLUE  + '!!!!!')
+time.sleep(1)
+print(Fore.BLACK + '!!!!!          !!!!!      '      + Fore.BLUE  + '!!!!!')
+
+print(Fore.BLACK + '!!!!!         !!!!!       '      + Fore.BLUE  + '!!!!!')
+time.sleep(1)
+print(Fore.BLACK + '!!!!!        !!!!!        '      + Fore.BLUE  + '!!!!!')
+
+print(Fore.BLACK + '!!!!!       !!!!!         '      + Fore.BLUE  + '!!!!!')
+time.sleep(1)
+print(Fore.BLACK + '!!!!!      !!!!!          '      + Fore.BLUE  + '!!!!!')
+
+print(Fore.BLACK + '!!!!!     !!!!!           '      + Fore.BLUE  + '!!!!!')
+time.sleep(1)
+print(Fore.BLACK + '!!!!!    !!!!!            '      + Fore.BLUE  + '!!!!!')
+
+print(Fore.BLACK + '!!!!!   !!!!!             '      + Fore.BLUE  + '!!!!!')
+time.sleep(1)
+print(Fore.BLACK + '!!!!!    !!!!!            '      + Fore.BLUE  + '!!!!!')
+
+print(Fore.BLACK + '!!!!!     !!!!!           '      + Fore.BLUE  + '!!!!!')
+time.sleep(1)
+print(Fore.BLACK + '!!!!!      !!!!!          '      + Fore.BLUE  + '!!!!!')
+
+print(Fore.BLACK + '!!!!!       !!!!!         '      + Fore.BLUE  + '!!!!!')
+time.sleep(1)
+print(Fore.BLACK + '!!!!!        !!!!!        '      + Fore.BLUE  + '!!!!!')
+time.sleep(1)
+print(Fore.BLACK + '!!!!!         !!!!!       '      + Fore.BLUE  + '!!!!!!!!!!!!!!!!!!!!')
+time.sleep(1)
+print(Fore.BLACK + '!!!!!          !!!!!      '      + Fore.BLUE  + '!!!!!!!!!!!!!!!!!!!!')
+print(Fore.BLUE + '  ')
+time.sleep(2)
+print(Fore.BLUE + '!!!!!!!!!!!!!!!!!!!!!     '       + Fore.BLACK  +   '!!!!!')
+time.sleep(1)
+print(Fore.BLUE + '!!!!!!!!!!!!!!!!!!!!!     '       + Fore.BLACK  +   '!!!!!')
+time.sleep(1)
+print(Fore.BLUE + '       !!!!!!             '       + Fore.BLACK   +  '!!!!!')
+time.sleep(1)
+print(Fore.BLUE + '       !!!!!!             '       + Fore.BLACK   +  '!!!!!')
+
+print(Fore.BLUE + '       !!!!!!             '       + Fore.BLACK   +  '!!!!!')
+time.sleep(1)
+print(Fore.BLUE + '       !!!!!!             '       + Fore.BLACK   +  '!!!!!')
+
+print(Fore.BLUE + '       !!!!!!             '       + Fore.BLACK   +  '!!!!!')
+time.sleep(1)
+print(Fore.BLUE + '       !!!!!!             '       + Fore.BLACK   +  '!!!!!')
+
+print(Fore.BLUE + '       !!!!!!             '       + Fore.BLACK   +  '!!!!!')
+time.sleep(1)
+print(Fore.BLUE + '       !!!!!!             '       + Fore.BLACK   +  '!!!!!')
+
+print(Fore.BLUE + '       !!!!!!             '       + Fore.BLACK   +  '!!!!!')
+time.sleep(1)
+print(Fore.BLUE + '       !!!!!!             '       + Fore.BLACK   +  '!!!!!')
+time.sleep(1)
+print(Fore.BLUE + '!!!!!!!!!!!!!!!!!!!!!     '       + Fore.BLACK  +   '!!!!!!!!!!!!!!!!!!!!')
+time.sleep(1)
+print(Fore.BLUE + '!!!!!!!!!!!!!!!!!!!!!     '       + Fore.BLACK   +  '!!!!!!!!!!!!!!!!!!!!')
+time.sleep(1)
+print(Fore.YELLOW + 'KILL 1.0 2024 Version')
+time.sleep(1)
+print(Fore.CYAN + 'BY IAN SANDI & CHRIS FARLEY')
+time.sleep(2)
+#-------------------------------------------------BANNER-------------------------------------------------------------------------
+print('')
+print(Fore.LIGHTRED_EX + "1) screen" + " 2)Exit")
+print('')
+time.sleep(1)
+choice = int(input(Fore.CYAN + "What program would you like to run" + Fore.LIGHTRED_EX + "? "))
+
+if((choice == 1)):
+  os.system('python3 screen.py')
+elif((choice == 2)):
+    os.system('clear')
+    time.sleep(1)
+    os.system('cd')
+    os.system('pwd')
+else: 
+    print(Fore.CYAN + "Invalid Input")
+    time.sleep(1)
+    os.system('cd')
+    print(Fore.CYAN + "Try Again.")
+    time.sleep(1)
+    os.system('python3 KILL.py')
